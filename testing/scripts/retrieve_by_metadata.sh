357ff73bfd9f44e42d71352f5034ea4876906ed0
eoinor-da <eoinor@decisionanalytics.ai>
2024-11-15 09:59:38 +0000

    changes

--- testing/scripts/retrieve_by_metadata.sh
+++ testing/scripts/retrieve_by_metadata.sh
@@ -6,7 +6,7 @@
 
 log_file="../logs/test.log"
 
-curl -G "http://localhost:8000/files" --data-urlencode "category=Dogs" --data-urlencode "subject=Otis" -o ../output/matched_files.zip \
+curl -G "http://localhost:8000/files" --data-urlencode "category=Dogs" --data-urlencode "subject=Otis" -o ../output/Otis.zip \
     -w "\nResponse Code: %{http_code}\n" -o - >> "$log_file" 2>&1
 
 echo "Logged on $(date)" >> "$log_file"
